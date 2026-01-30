16f557ba100596bb446fad38c0164ca2159284d6
Fachri Akbar Kutubi <fachriakbarkutubi@gmail.com>
2026-01-31 03:01:07 +0700

    Enable withCredentials for API requests

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import { getItemWithExpiry } from "./storage";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
